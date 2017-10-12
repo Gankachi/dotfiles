fa76e325dbbbecc51a1632def5a9b7e03090931e
Kévin Cuallado-Keltsch <kevin.cuallado@etu.enseeiht.fr>
2017-10-12 07:28:37 +0200

    Remove leftover files

--- .i3/lib/temp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Displays the CPU Temperature
-# Eg: Temp: +31.0°
-
-echo "<span background='#2aa198' foreground='#073642'></span><span background='#073642' foreground='#fdf6e3'> $(sensors coretemp-isa-0000 | awk '/Physical/ {print $4}') </span>"
